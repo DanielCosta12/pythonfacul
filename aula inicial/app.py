80148168fc08b8cf4794b3c78624cd74d35f9c98
Daniel Costa <danieldossantosdacosta@gmail.com>
2023-08-07 14:39:34 -0300

    primeiro commit com alguns exemplos usados(simples)

---
+++ aula inicial/app.py
@@ -0,0 +1,20 @@
+print("Olá mundo!")
+idade = 18; 
+print(idade)
+aluno = "Daniel"
+nota = 6.5
+x = 10
+flag = True
+print(type(flag))
+print(type(aluno))
+print(type(nota))
+print(type(x))
+
+name = input("Digite um nome: ")
+print("Olá %s, bem vindo futuro dev em python!"% (name))
+print("Olá {} essa é uma outra forma de fazer".format(name))
+
+if name =="Daniel":
+    print("VOCÊ É BRABO!")
+else:
+    print("VOCÊ NÃO É BRABO!")
